642ae9136d4e633987d7e96a8264e84b73a9750f
jkolb <franticapparatus@gmail.com>
2014-12-03 22:55:44 -0600

    Initial Commit

---
+++ FieryCrucible/FieryCrucible.h
@@ -0,0 +1,19 @@
+//
+//  FieryCrucible.h
+//  FieryCrucible
+//
+//  Created by Justin Kolb on 12/3/14.
+//  Copyright (c) 2014 Justin Kolb. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FieryCrucible.
+FOUNDATION_EXPORT double FieryCrucibleVersionNumber;
+
+//! Project version string for FieryCrucible.
+FOUNDATION_EXPORT const unsigned char FieryCrucibleVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FieryCrucible/PublicHeader.h>
+
+
